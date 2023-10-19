ba56aa4b010cce3cbddc263e65ff6ea598c67a4a
bombonicamei <122697984+bombonicamei@users.noreply.github.com>
2023-10-19 15:25:53 +0300

    Update speech.java

--- speech.java
+++ speech.java
@@ -4,10 +4,10 @@ var recognition = new webkitSpeechRecognition();
 recognition.lang = 'en-US';
 function on_touch()
 {
-    if(recognition.start){
-       recognition.start();
-       recognition_started = true;
-      }
+if(recognition.start){
+recognition.start();
+recognition_started = true;
+}
 }
 function onend()
 {
@@ -19,4 +19,5 @@ recognition.onsoundend = onend;
 recognition.onspeechend = onend;
 recognition.onresult =on_results;
 function on_results(e)
-{document.getElementById("text").innerHTML += "Ati rostit cuvantul: "+ e.results[0][0].transcript + ", acuratete: "+e.results[0][0].confidence + "<br>"}
+{
+document.getElementById("text").innerHTML += "Ati rostit cuvantul: "+ e.results[0][0].transcript + ", acuratete: "+e.results[0][0].confidence + "<br>"}
